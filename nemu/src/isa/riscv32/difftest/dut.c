9f9aeba9f3ca6e42bb6901c4f20ac041d2afbd8d
24120012-Miller <1098721429@qq.com>
2025-01-30 13:10:39 +0800

    Implement difftest func

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,23 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for(int i = 0; i < 32; i++)
+  {
+    if(ref_r->gpr[i] != cpu.gpr[i])
+    {
+      printf("reg[%d] is different after executing instruction at pc = " FMT_WORD ", right = " FMT_WORD ", wrong = " FMT_WORD ", diff = " FMT_WORD "\n",
+        i, pc, ref_r->gpr[i], cpu.gpr[i], ref_r->gpr[i] ^ cpu.gpr[i]);
+      return false;
+    }
+  }
+  if(ref_r->pc != cpu.pc)
+  {
+    printf("pc is different after executing instruction at pc = " FMT_WORD ", right = " FMT_WORD ", wrong = " FMT_WORD ", diff = " FMT_WORD "\n",
+      pc, ref_r->pc, cpu.pc, ref_r->pc ^ cpu.pc);
+    return false;
+  }
+
+  return true;
 }
 
 void isa_difftest_attach() {
--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,9 +25,13 @@ const char *regs[] = {
 
 void isa_reg_display() {
   int i = 0;
-  for(i = 0; i < 32; i++) {
-    printf("%3s:0x%x\n",regs[i], cpu.gpr[i]);
+  // for(i = 0; i < 32; i++) {
+  //   printf("%3s:0x%x\n",regs[i], cpu.gpr[i]);
+  // }
+  for(i = 0; i < 32; i++){
+    printf("%-4s    0x%08x\n", regs[i], cpu.gpr[i]);
   }
+  printf("pc      0x%8x\n", cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
